19bbb1a361381a57e91d660e7281a62b97badfb0
Wooyoung.Choi <andy241822@gmail.com>
2026-02-15 02:46:52 +0900

    fix: url

--- tests/e2e/case/test_todo.py
+++ tests/e2e/case/test_todo.py
@@ -1,5 +1,7 @@
+import os
+
 from playwright.sync_api import expect
 
 
 def test_todo_url(todo_page):
-    expect(todo_page).to_have_url('https://black-stone-05a57af00.2.azurestaticapps.net/')
+    expect(todo_page).to_have_url(os.getenv('PAGE_URL'))
