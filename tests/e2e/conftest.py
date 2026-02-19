bcd48c6d2ffedb3511a5f2499af417411f5f13eb
onyu.park <onyu.park@wonderwall.kr>
2026-02-19 10:34:11 +0900

    test: 페이지 접속 및 로그인 세션 e2e 테스트 추가

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 
 from playwright.sync_api import Playwright
 import pytest
@@ -6,7 +7,8 @@ import pytest
 from tests.e2e.utils.playwright_config import playwright_config_base, playwright_tear_down_base
 
 HEADLESS = False
-PAGE_URL = os.getenv('PAGE_URL')
+PAGE_URL = os.getenv('PAGE_URL', 'https://black-stone-05a57af00.2.azurestaticapps.net/')
+LOGIN_URL = f'{PAGE_URL}login'
 
 
 @pytest.fixture(scope='session')
@@ -16,8 +18,29 @@ def browser(playwright: Playwright):
     browser.close()
 
 
+@pytest.fixture(scope='session')
+def web_session_driver(browser, playwright: Playwright):
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto(PAGE_URL, wait_until='domcontentloaded')
+    page.wait_for_url(LOGIN_URL)
+    page.fill('#login-username', 'admin')
+    page.fill('#login-password', 'admin1!')
+    page.click('button.login-btn')
+    page.wait_for_url(PAGE_URL)
+
+    storage_file = tempfile.mktemp(suffix='.json')
+    context.storage_state(path=storage_file)
+    context.close()
+
+    yield storage_file
+
+    os.remove(storage_file)
+
+
 @pytest.fixture
-def your_page_or_driver_name(browser, playwright: Playwright, request, web_session_driver):
+def todo_onyu_page(browser, playwright: Playwright, request, web_session_driver):
     no_cookie = request.node.get_closest_marker('no_cookie')
 
     storage = None if no_cookie else web_session_driver
