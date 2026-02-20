2d47e37597e1bb7cf4b574fe6ba6e5eaeff1a3ee
jinsollee <jinsollee78@gmail.com>
2026-02-20 10:14:22 +0900

    feat : 로그인 추가

--- tests/e2e/conftest.py
+++ tests/e2e/conftest.py
@@ -19,12 +19,30 @@ def browser(playwright: Playwright):
     browser.close()
 
 
+@pytest.fixture(scope='session')
+def logged_in(browser):
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto(PAGE_URL)
+
+    id_input = page.locator('#login-username')
+    pw_input = page.locator('#login-password')
+
+    id_input.fill('admin')
+    pw_input.fill('admin1!')
+
+    page.click("button[type='submit']")
+
+    yield context
+
+    context.close()
+
+
 @pytest.fixture
 def jinsol_test(browser, playwright: Playwright, request):
     # no_cookie = request.node.get_closest_marker('no_cookie')
-
     # storage = None if no_cookie else web_session_driver
-
     page, context, browser = playwright_config_base(playwright, PAGE_URL, browser, headless=HEADLESS)
 
     try:
